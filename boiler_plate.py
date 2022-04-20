fb41840d3a4117ac608a6d5d4047a4200c54c524
afv14 <97483119+afv14@users.noreply.github.com>
2022-04-20 17:48:02 -0400

    Adjustments

--- boiler_plate.py
+++ boiler_plate.py
@@ -1,3 +1,9 @@
 from pprint import pprint
-from Google import Create_Service
+from google import Create_Service
 
+CLIENT_SECRET_FILE = ALEXA_JSON
+API_NAME = 'calendar'
+API_VERSION = 'v3'
+SCOPES =['https://www.googleapis.com/auth/calendar']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
